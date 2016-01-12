6c041f9522f95c13df84235964708849babf86cc
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2016-01-12 22:52:03 +0000

    Changed port

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json())
 app.set("jsonp callback", true);
 require('./routes/routes')(app);
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
